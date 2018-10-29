a4a809f7a13071cd959d775e709ca0abdf144d17
Dev <jbabak@Samis-MacBook-Pro.local>
2018-10-29 17:05:32 -0400

    Updated lando file and removed intial files for marketplacekit

--- app/Models/Permission.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Permission extends \Spatie\Permission\Models\Permission
-{
-    use \Spiritix\LadaCache\Database\LadaCacheTrait;
-
-}
--- app/Models/Role.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class Role extends \Spatie\Permission\Models\Role
-{
-    use \Spiritix\LadaCache\Database\LadaCacheTrait;
-
-}
